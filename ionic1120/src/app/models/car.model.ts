44a0239baa11ae98acc2cc975b12a973aff6a12b
Vincent <vleclerc@inow.fr>
2020-11-09 15:39:24 +0100

    add cars

---
+++ ionic1120/src/app/models/car.model.ts
@@ -0,0 +1,8 @@
+export class CarModel {
+    id: number;
+    model: string;
+    price: number;
+    dateOfCirculation: Date;
+    mark: any;
+    markID: number;
+}
---
+++ ionic1120/src/app/services/car.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CarModel } from '../models/car.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CarService {
+    constructor(private _http: HttpClient) { }
+
+    getCars(): Observable<Array<CarModel>> {
+        return this._http.get<Array<CarModel>>(`${environment.urlAPI}cars`);
+    }
+
+    addCar(car: CarModel): Observable<CarModel> {
+        return this._http.post<CarModel>(`${environment.urlAPI}cars`, car);
+    }
+}
