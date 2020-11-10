321f819d68d9de37adca2cf394b22c60aa3d5778
Vincent <vleclerc@inow.fr>
2020-11-10 16:05:51 +0100

    add camera

--- ionic1120/src/app/tab3/tab3.page.ts
+++ ionic1120/src/app/tab3/tab3.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CameraResultType, CameraSource, Plugins } from '@capacitor/core';
+//import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  base64Image;
+  constructor() { }
 
-  constructor() {}
+  async ngOnInit() {
+
+    const image = await Camera.getPhoto({
+      quality: 100,
+      width: 400,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    this.base64Image = image;
+  }
 
 }
